cc4ffb8fcc93498a177c58ae9732ede5ec85f58a
skaltus <gaydaev.mergen@gmail.com>
2021-02-05 18:04:44 +0300

    feat: add docker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import Koa from 'koa';
+import config from './common/config';
 
+const app = new Koa();
+const { PORT } = config;
+
+app.use((ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(PORT, () => console.log(`The app is launched in ${app.env} mode on port ${PORT}`));
